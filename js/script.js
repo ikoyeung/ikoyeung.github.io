0979c30e28639b267df8303e742f6adac187c5ff
Yeung <@Yeung>
2024-10-19 00:28:14 +0900

    test header-and-footer-js 0:28

--- js/script.js
+++ js/script.js
@@ -1,53 +1,25 @@
-const getElements = () => {
-  const nav = document.querySelector(".navbar");
-  const menuBtn = document.querySelector("#menu-btn");
+// const getElements = () => {
+//   const nav = document.querySelector(".navbar");
+//   const menuBtn = document.querySelector("#menu-btn");
 
-  menuBtn.addEventListener("click", () => {
-    nav.classList.toggle("active");
-  });
+//   menuBtn.addEventListener("click", () => {
+//     nav.classList.toggle("active");
+//   });
 
-  window.addEventListener("scroll", () => {
-    nav.classList.remove("active");
-  });
-};
-
-const fetchHeader = async () => {
-  try {
-    const res = await fetch("../header.html");
-    const template = await res.text();
-    const parse = new DOMParser();
-    const html = parse.parseFromString(template, "text/html");
-    const header = html.querySelector("body header");
-
-    document.body.prepend(header);
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-const fetchFooter = async () => {
-  try {
-    const res = await fetch("../footer.html");
-    const template = await res.text();
-    const parse = new DOMParser();
-    const html = parse.parseFromString(template, "text/html");
-    const footer = html.querySelector("body footer");
-
-    document.body.append(footer);
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-// const headerElement = document.querySelector("header");
-// const footerElement = document.querySelector("footer");
+//   window.addEventListener("scroll", () => {
+//     nav.classList.remove("active");
+//   });
+// };
 
 // const fetchHeader = async () => {
 //   try {
-//     const res = await fetch("../header.txt");
+//     const res = await fetch("../header.html");
 //     const template = await res.text();
+//     const parse = new DOMParser();
+//     const html = parse.parseFromString(template, "text/html");
+//     const header = html.querySelector("body header");
 
-//     headerElement.innerHTML = template;
+//     document.body.prepend(header);
 //   } catch (err) {
 //     console.log(err);
 //   }
@@ -55,14 +27,42 @@ const fetchFooter = async () => {
 
 // const fetchFooter = async () => {
 //   try {
-//     const res = await fetch("../footer.txt");
+//     const res = await fetch("../footer.html");
 //     const template = await res.text();
+//     const parse = new DOMParser();
+//     const html = parse.parseFromString(template, "text/html");
+//     const footer = html.querySelector("body footer");
 
-//     footerElement.innerHTML = template;
+//     document.body.append(footer);
 //   } catch (err) {
 //     console.log(err);
 //   }
 // };
+
+const headerElement = document.querySelector("header");
+const footerElement = document.querySelector("footer");
+
+const fetchHeader = async () => {
+  try {
+    const res = await fetch("../header.txt");
+    const template = await res.text();
+
+    headerElement.innerHTML = template;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const fetchFooter = async () => {
+  try {
+    const res = await fetch("../footer.txt");
+    const template = await res.text();
+
+    footerElement.innerHTML = template;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 fetchHeader().then(getElements);
 fetchFooter();
